1fd95a58c07271b5ac7651bd3b4dcfcd9f0f9731
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2020-12-10 13:21:13 +0000

    refactered code to use fs-extra from a new utlilites file

--- src/projects/index.js
+++ src/projects/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const fs = require("fs");
+const { readDB, writeDB } = require("../lib/utilities");
 const path = require("path");
 const uniqid = require("uniqid");
 const { check, validationResult } = require("express-validator");
@@ -7,16 +7,9 @@ const router = express.Router();
 
 const projectFilePath = path.join(__dirname, "projects.json"); //GETTING FILEPATH TO JSON
 
-const getFile = () => {
-  //WE'RE MAKING THIS FUNCTION FOR FUTURE REFACTORING
-  const machineBuffer = fs.readFileSync(projectFilePath); // TAKES THE FILE AND TURNS IT TO MACHINE CODE
-  const fileString = machineBuffer.toString(); //TAKES MACHINE CODE AND TURNS IT TO A JSON STRING
-  return JSON.parse(fileString); //TURNS JSON STRING INTO JSON ARRAY WE CAN CODE WITH, AND RETURNS IT FROM THE FUNCTION
-};
-
-router.get("/", (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    const projectDataBase = getFile(); //RUNS FUNCTION TO GET DATABASE
+    const projectDataBase = await readDB(projectFilePath); //RUNS FUNCTION TO GET DATABASE
     if (projectDataBase.length > 0) {
       res.status(201).send(projectDataBase); //SENDS RESPONSE WITH GOOD CODE AND WHOLE DATABSE
     } else {
@@ -30,9 +23,9 @@ router.get("/", (req, res, next) => {
   }
 });
 
-router.get("/:id", (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   try {
-    const projectDataBase = getFile(); //RUNS FUNCTION TO GET DATABASE
+    const projectDataBase = await readDB(projectFilePath); //RUNS FUNCTION TO GET DATABASE
     const singleProject = projectDataBase.filter(
       (project) => project.ID === req.params.id
     );
@@ -68,8 +61,12 @@ router.post(
       .exists()
       .isLength({ min: 1 })
       .withMessage("You need to have a live demo of your project"),
+    check("StudentID")
+      .exists()
+      .isLength({ min: 1 })
+      .withMessage("You need to have your Student ID"),
   ],
-  (req, res, next) => {
+  async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       const err = {};
@@ -77,21 +74,20 @@ router.post(
       err.httpStatusCode = 400;
       next(err);
     } else {
-      const projectDataBase = getFile(); //RUNS FUNCTION TO GET DATABASE
+      const projectDataBase = await readDB(projectFilePath); //RUNS FUNCTION TO GET DATABASE
       const newProject = req.body; //GETS THE REQUEST BODY
       newProject.ID = uniqid(); //GIVES BODY NEW ID
-      newProject.StudentID = uniqid(); //GIVES BODY NEW STUDENT ID
       newProject.CreationDate = new Date(); //GIVES BODY CREATION DATE
       projectDataBase.push(newProject); //ADDS BODY TO DATABSE
-      fs.writeFileSync(projectFilePath, JSON.stringify(projectDataBase)); //OVERWRITES OLD DATABASE WITH NEW DATABASE
+      await writeDB(projectFilePath, projectDataBase); //OVERWRITES OLD DATABASE WITH NEW DATABASE
       res.status(201).send(projectDataBase); //SENDS RESPONSE WITH GOOD CODE AND WHOLE DATABSE
     }
   }
 );
 
-router.put("/:id", (req, res, next) => {
+router.put("/:id", async (req, res, next) => {
   try {
-    const projectDataBase = getFile(); //RUNS FUNCTION TO GET DATABASE
+    const projectDataBase = await readDB(projectFilePath); //RUNS FUNCTION TO GET DATABASE
     const singleProject = projectDataBase.filter(
       (project) => project.ID === req.params.id
     );
@@ -108,7 +104,7 @@ router.put("/:id", (req, res, next) => {
         ModifiedDate: new Date(),
       };
       filteredDB.push(editedProject);
-      fs.writeFileSync(projectFilePath, JSON.stringify(filteredDB));
+      await writeDB(projectFilePath, filteredDB);
       res.status(201).send(filteredDB); //SENDS RESPONSE WITH GOOD CODE AND WHOLE DATABSE
     } else {
       const err = {};
@@ -121,9 +117,9 @@ router.put("/:id", (req, res, next) => {
   }
 });
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", async (req, res, next) => {
   try {
-    const projectDataBase = getFile(); //RUNS FUNCTION TO GET DATABASE
+    const projectDataBase = await readDB(projectFilePath); //RUNS FUNCTION TO GET DATABASE
     const singleProject = projectDataBase.filter(
       (project) => project.ID === req.params.id
     );
@@ -131,7 +127,7 @@ router.delete("/:id", (req, res, next) => {
       const filteredDB = projectDataBase.filter(
         (project) => project.ID !== req.params.id
       );
-      fs.writeFileSync(projectFilePath, JSON.stringify(filteredDB));
+      await writeDB(projectFilePath, filteredDB);
       res.status(201).send(filteredDB); //SENDS RESPONSE WITH GOOD CODE AND WHOLE DATABSE
     } else {
       const err = {};
